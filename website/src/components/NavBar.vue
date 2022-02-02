e909455f3842904efafe6da3dfad65f29fc34e35
0xsapphir3 <0xsapphir3@gmail.com>
2022-02-02 18:24:56 +0530

    feat: Updates

---
+++ website/src/components/NavBar.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="_container h-16 flex items-center pl-20">
+    <div class="name text-2xl">Web Application</div>
+    <div class="items flex items-center absolute right-20">
+        <div class="item">Home</div>
+        <div class="item">About</div>
+        <div class="item">Languages</div>
+        <div class="item">Courses</div>
+        <div class="item">Feedback</div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    setup() {
+        
+    },
+}
+</script>
+
+<style scoped>
+._container {
+    background-color: #3B4252;
+    color: #D8DEE9;
+}
+
+.item {
+    margin-left: 40px;
+}
+
+.item:hover {
+    color: #88C0D0;
+    cursor: pointer;
+}
+</style>
--- website/src/views/AboutView.vue
+++ website/src/views/AboutView.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about flex items-center justify-center">
+        <div class="wrapper">
+            <div class="text-center text-5xl">Web Application</div><br><br>
+            <div class="text-center text-4xl">Once a student, Now a teacher</div>
+            <br><br>
+            <div class="flex">
+                <button class="p-4 rounded-lg ml-10 mr-10">Login</button>
+                <button class="p-4 rounded-lg ml-10 mr-10">Sign Up</button>
+            </div>
+        </div>
+    
+</div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.about {
+    height: 90vh;
+    color: #D8DEE9;
+}
+
+button {
+    background-color: #3B4252;
+    width: 50%;
 }
 </style>
