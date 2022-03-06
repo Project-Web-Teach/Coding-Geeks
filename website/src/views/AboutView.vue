afa3830a44c9c0690c47a408c9d48f4a4c31e0ef
0xsapphir3 <0xsapphir3@gmail.com>
2022-03-06 12:37:06 +0530

    bug: Fix wrong image endpoint

--- website/src/views/AboutView.vue
+++ website/src/views/AboutView.vue
@@ -3,7 +3,7 @@
 <div class="_container">
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
         <div class="container">
-            <a class="navbar-brand" href="#"><img src="assets/CodingGeeksLogo.png" width="80" height="35" alt="Coding Geeks"></a>
+            <a class="navbar-brand" href="#"><img src="../assets/CodingGeeksLogo.png" width="80" height="35" alt="Coding Geeks"></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
